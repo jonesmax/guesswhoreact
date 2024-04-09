50c6d72d84490fb50d57cc90ab45bf532e7b0260
Maxwell Jones <maxjones2001@hotmail.com>
2024-04-08 20:44:56 -0400

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Car from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Car/>
+    </div>
+  );
+}
+
+export default App;
